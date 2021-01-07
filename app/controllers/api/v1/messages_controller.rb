7c07358f8409c3573078575171160a860ab46835
Robert Chapman <robchapman94@gmail.com>
2021-01-07 23:53:14 +0000

    Completed Anon posting feature

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::MessagesController < ApplicationController
   before_action :set_channel
+  before_action :set_anon_user, only: :create
 
   def index
     messages = Message.where("channel_id = #{@channel.id}").order(created_at: :asc)
     messages_new = messages.map do |message|
       {
         "id": message.id,
-        "author": message.user.nickname,
+        "author": message.user&.nickname || message.anon_user&.nickname || "anon",
         "content": message.content,
         "created_at": message.created_at
       }
@@ -17,16 +18,16 @@ class Api::V1::MessagesController < ApplicationController
   def create
     # TODO: Validations on message content format
     # TODO: allow anon chatting
-    message = Message.create(content: message_params[:content], user: current_user, channel: @channel)
+    message = Message.create(content: message_params[:content], user: current_user, channel: @channel, anon_user: @anon_user)
     message_new = {
       "id": message.id,
-      "author": current_user ? message.user.nickname : cookies[:anonNickname] || 'Anon',
+      "author": message.user&.nickname || message.anon_user&.nickname || "anon",
       "content": message.content,
       "created_at": message.created_at
     }
     ActionCable.server.broadcast 'chat_channel',
       "id": message.id,
-      "author": message.user.nickname,
+      "author": message.user&.nickname || message.anon_user&.nickname || "anon",
       "content": message.content,
       "created_at": message.created_at
     render json: message_new
@@ -40,5 +41,18 @@ class Api::V1::MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def publish_message(message)
+    {
+      "id": message.id,
+      "author": message.user&.nickname || message.anon_user&.nickname || "anon",
+      "content": message.content,
+      "created_at": message.created_at
+    }
+  end
+
+  def set_anon_user
+    @anon_user = AnonUser.find_by(nickname: cookies[:anonNickname])
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user, optional: true
+  belongs_to :anon_user, optional: true
   belongs_to :channel
 end
