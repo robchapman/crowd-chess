b3ded792f11bac07209b7066d3dcfe114dffc06e
Robert Chapman <robchapman94@gmail.com>
2021-02-01 21:49:49 +0000

    Game Bar Component plasyer labels

--- app/javascript/chess/containers/game_bar.jsx
+++ app/javascript/chess/containers/game_bar.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import Clock from '../containers/clock';
 import TeamBanner from '../containers/team_banner';
@@ -7,12 +9,31 @@ class GameBar extends Component {
   render() {
     return (
       <div className="game-bar">
+        <div className="game-bar-upper">
           <TeamBanner team="white" />
           <Clock />
           <TeamBanner team="black"/>
+        </div>
+        <div className="game-bar-lower">
+          <div className="player-stats">
+            <p>{`White: ${0} players`}</p>
+            <p>{`Playing as: ${this.props.playerTeam}`}</p>
+            <p>{`Black: ${0} players`}</p>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default GameBar;
+function mapStateToProps(state) {
+  return {
+    playerTeam: state.playerTeam
+  };
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({  }, dispatch);
+// }
+
+export default connect(mapStateToProps, null)(GameBar);
